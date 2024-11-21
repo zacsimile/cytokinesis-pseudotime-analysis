c84a2f54cc6e74628921c7d52ae372651bade856
NadjaHuempfer <n.huempfer@fu-berlin.de>
2024-11-21 15:37:51 +0100

    simplify the way the data is loaded

--- image_utils.py
+++ image_utils.py
@@ -13,7 +13,7 @@ def pad_rot_and_trans_im(im, angle, x, y, N=2048):
     Parameters
     ----------
     im : np.typing.ArrayLike
-        Image (three-color).
+        Image (CYX).
     angle : float
         Angle to rotate the image in degrees.
     x, y : float
@@ -25,10 +25,10 @@ def pad_rot_and_trans_im(im, angle, x, y, N=2048):
     if np.any([N < x for x in im.shape]):
         raise Exception("Padding is smaller than image size. Increase N.")
     if len(im.shape) != 3:
-        raise Exception("This only works for images of size 3. TODO: Extend.")
+        raise Exception("This only works for images of dimension 3. TODO: Extend.")
 
     # Pad the image
-    im_pad = np.zeros((3,N,N), dtype=im.dtype)
+    im_pad = np.zeros((im.shape[0],N,N), dtype=im.dtype)
     x_off, y_off = (N-im.shape[1])//2, (N-im.shape[2])//2
     im_pad[:,x_off:(x_off+im.shape[1]),y_off:(y_off+im.shape[2])] = im
 
